d0ae10642151a4349898cfcde275fdd4f075ac39
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-12-10 09:27:27 -0300

    Adjustment commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,31 +9,37 @@
 // import { AlertTaskDemo } from "@/components/shadcn-studio/alert/alert-09";
 // import { Login } from "@/components/shadcn-studio/blocks/login-page-02/login-page-02";
 // import { ComboboxMultipleDemo } from "@/components/shadcn-studio/combobox/combobox-10";
-import HeroSectionPage from "./hero-section-25/page";
+// import HeroSectionPage from "./hero-section-25/page";
+import Header1 from "@/components/smoothui/header-1";
 
 const Home = () => {
-    // return Login();
-    // return SSAlertDemo();
-    // return ComboboxMultipleDemo();
-    // return HeroSectionPage();
-    return <HeroSectionPage />;
-    // return (
-    //     <div className="flex items-center justify-center min-h-screen">
-    //         <HeroSectionPage />
-    //     </div>
-    // <div className="relative min-h-screen bg-background">
-    //     {" "}
-    //     {/* relative aqui */}
-    //     {/* ModeToggle posicionado no canto superior direito */}
-    //     <div className="absolute top-4 right-4 z-20">
-    //         <ModeToggle />
-    //     </div>
-    //     {/* Card centralizado */}
-    //     <div className="flex items-center justify-center min-h-screen">
-    //         {/*<HeroSectionPage />*/}
-    //     </div>
-    // </div>
-    // );
+  // return Login();
+  // return SSAlertDemo();
+  // return ComboboxMultipleDemo();
+  // return HeroSectionPage();
+  // return <HeroSectionPage />;
+  return (
+    <div className="h-full w-full">
+      <Header1 />
+    </div>
+  );
+  // return (
+  //     <div className="flex items-center justify-center min-h-screen">
+  //         <HeroSectionPage />
+  //     </div>
+  // <div className="relative min-h-screen bg-background">
+  //     {" "}
+  //     {/* relative aqui */}
+  //     {/* ModeToggle posicionado no canto superior direito */}
+  //     <div className="absolute top-4 right-4 z-20">
+  //         <ModeToggle />
+  //     </div>
+  //     {/* Card centralizado */}
+  //     <div className="flex items-center justify-center min-h-screen">
+  //         {/*<HeroSectionPage />*/}
+  //     </div>
+  // </div>
+  // );
 };
 
 export default Home;
